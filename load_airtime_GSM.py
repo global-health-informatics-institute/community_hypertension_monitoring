4cd0f319da90f84e65053d2075d49cf05b04f32e
Gerrlad Simwaka <gerrladsimwaka@gmail.com>
2019-11-12 09:41:48 +0200

    top-up airtime.

---
+++ load_airtime_GSM.py
@@ -0,0 +1,13 @@
+import serial
+import os, time
+
+def load_airtime():
+    # Enable Serial Communication
+    ser_port = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=1)
+
+    # Delete all sms in GSM
+    ser_port.write(str.encode('AT+cusd=1,"*136*_____________#"'+'\r\n'))            
+    read_port = ser_port.read(10)
+    time.sleep(0.1)
+
+delete_all_GSM()
